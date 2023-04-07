6b9adbc6aef4fe5f2d0c07d987e081d20f326c56
EA-Gadgeter <emiacevedoposos@gmail.com>
2023-04-06 23:06:43 -0600

    Fixed manifest.json

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,7 +6,7 @@ export default function Document() {
       <Html lang="en">
         <Head>
           <meta charSet='utf-8'/>
-          <link rel='manifest' href='/manifest.html'/>
+          <link rel='manifest' href='/manifest.json'/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
